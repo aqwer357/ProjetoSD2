84159a0c4956e1ea86adaf3bde00421031bbfeb0
Artur Cassimiro <47155261+aqwer357@users.noreply.github.com>
2019-12-09 11:21:21 -0300

    Add files via upload

---
+++ CPU-sd2/CPU.v
@@ -0,0 +1,7 @@
+module CPU(tx, ty, tz, tula, acumulador, barramento, ulaout, entrada, clock);
+	input wire clock;
+	
+	output [3:0] out;
+	output [3:0] tx, ty, tz, tula, acumulador, barramento, ulaout, entrada;
+	
+endmodule
